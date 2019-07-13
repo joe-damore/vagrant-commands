a4f9396944008375433ab1bc348b7b24744651a5
Joe D'Amore <joe@joedamore.me>
2019-07-12 20:46:38 -0400

    Remove default gem lib source

--- lib/vagrant/commands.rb
+++
@@ -1,8 +0,0 @@
-require "vagrant/commands/version"
-
-module Vagrant
-  module Commands
-    class Error < StandardError; end
-    # Your code goes here...
-  end
-end
--- lib/vagrant/commands/version.rb
+++
@@ -1,5 +0,0 @@
-module Vagrant
-  module Commands
-    VERSION = "0.1.0"
-  end
-end
