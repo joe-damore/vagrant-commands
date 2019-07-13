7fc7a75bb82484771e99942902450c9d4b7198e7
Joe D'Amore <joe@joedamore.me>
2019-07-12 20:31:52 -0400

    Add Gem starter files

---
+++ lib/vagrant/commands.rb
@@ -0,0 +1,8 @@
+require "vagrant/commands/version"
+
+module Vagrant
+  module Commands
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ lib/vagrant/commands/version.rb
@@ -0,0 +1,5 @@
+module Vagrant
+  module Commands
+    VERSION = "0.1.0"
+  end
+end
